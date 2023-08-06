cd8068c46e7e1113eb2e888fabf35841b64100fa
Flavio Silva <flavio.silva@acal.local>
2023-08-06 08:31:13 -0300

    feat: create README.md

---
+++ README.md
@@ -0,0 +1,20 @@
+# Javascript stubs
+
+Implementação de stubs para tests com nodejs.
+
+como baixar o projeto?
+
+```shell
+git clone https://github.com/flavicon/javascript-stubs.git
+
+cd javascript-stubs
+```
+
+como executar o projeto?
+
+> Certifique-se de que o nodejs está instalado na sua máquina.
+> [Baixar nodejs](https://nodejs.org/en/download)
+
+```shell
+npm run test
+```
--- src/service.test.js
+++ src/service.test.js
@@ -2,12 +2,12 @@ const sinon = require("sinon");
 const Service = require("./service");
 const { deepStrictEqual } = require("assert");
 
-const BASE_URL_1 = "https://swapi.dev/api/planets/1/";
-const BASE_URL_2 = "https://swapi.dev/api/planets/2/";
 const mocks = {
     tatooine: require("../mocks/tatooine.json"),
     alderaan: require("../mocks/alderaan.json"),
 };
+const BASE_URL_1 = "https://swapi.dev/api/planets/1/";
+const BASE_URL_2 = "https://swapi.dev/api/planets/2/";
 
 (async () => {
     const service = new Service();
